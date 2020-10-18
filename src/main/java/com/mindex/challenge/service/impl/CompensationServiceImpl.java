f526aafb61cddd718ac9a9502a31f5b131aa067a
Nevets44 <sds9278@g.rit.edu>
2020-10-18 14:32:32 -0400

    Adding Service annotation to the new classes services so that project will properly run

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.service.CompensationService;
 
+@Service
 public class CompensationServiceImpl implements CompensationService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CompensationServiceImpl.class);
--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureImpl.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.ReportingStructureService;
 
+@Service
 public class ReportingStructureImpl implements ReportingStructureService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureImpl.class);
